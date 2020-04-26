32939a7b3350139e32707a0533b1e63b1699ee5b
Bruno Assis <bruno.assis@originalapp.com.br>
2020-04-26 08:20:00 -0300

    novas buscas feitas no banco de dados mysql e h2

--- src/main/java/com/springjpa/userest/config/dataInitilizr.java
+++ src/main/java/com/springjpa/userest/config/dataInitilizr.java
@@ -1,5 +1,7 @@
 package com.springjpa.userest.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -9,17 +11,45 @@ import com.springjpa.userest.entity.User;
 import com.springjpa.userest.repository.UserRepository;
 
 @Component
-public class dataInitilizr implements ApplicationListener<ContextRefreshedEvent>{
+public class dataInitilizr implements ApplicationListener<ContextRefreshedEvent> {
 
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
+		List<User> users = this.userRepository.findAll();
+
+		if (users.isEmpty()) {
+			this.createUser("Bruno", "bruno@email.com");
+			this.createUser("Gracie", "Gracie@email.com");
+			this.createUser("Gracie2", "Gracie2@email.com");
+		}
 		
-		User user = User.builder().name("Bruno").email("bruno@email.com").build();
+//		this.getSecondUser();
+//		this.deleteById(1L);
+		
+		User user = this.findByEmail("bruno@email.com");
+		System.out.println(user.getName());
+	}
+
+	private void createUser(String name, String email) {
+		User user = User.builder().name(name).email(email).build();
 
 		this.userRepository.save(user);
+	}
+
+	private void getSecondUser() {
+		User user = this.userRepository.getOne(2L);
+		System.out.println(user.getName());
+	}
+
+	private void deleteById(Long id) {
+		this.userRepository.getOne(id);
+	}
+	
+	private User findByEmail(String email) {
+		return this.userRepository.findBySomeEmail(email);
 	}
 
 }
--- src/main/java/com/springjpa/userest/repository/UserRepository.java
+++ src/main/java/com/springjpa/userest/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.springjpa.userest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.springjpa.userest.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	
+	User findByName(String name);
+	
+	@Query("select u from User u where u.email like %?1%")
+	User findBySomeEmail(String email);
 }
